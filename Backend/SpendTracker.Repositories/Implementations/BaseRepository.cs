f62a7320501409345f778d31fe6a69fdd036f7cb
Ersin Uluağaç <ersinuluagac@gmail.com>
2026-01-04 14:02:13 +0300

    feat(backend): Add BaseRepository methods (FindByCondition, Create, Update, Delete) with comment lines.

--- Backend/SpendTracker.Repositories/Implementations/BaseRepository.cs
+++ Backend/SpendTracker.Repositories/Implementations/BaseRepository.cs
@@ -1,22 +1,49 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using SpendTracker.Repositories.Contexts;
 using SpendTracker.Repositories.Interfaces;
+using System.Linq.Expressions;
 
 namespace SpendTracker.Repositories.Implementations;
 
 public class BaseRepository<T> : IBaseRepository<T> where T : class
 {
+    // Fields (Bağımlılıkların tanımlandığı yer)
     protected readonly AppDbContext _context;
 
+    // Constructor (Bağımlılıkların enjekte edildiği yer)
     public BaseRepository(AppDbContext context)
     {
         _context = context;
     }
 
+    // Methods
+    #region Read Methods (Veri okuma, sorgulama işlemleri
     public IQueryable<T> FindAll(bool trackChanges)
     {
         return !trackChanges
             ? _context.Set<T>().AsNoTracking()
             : _context.Set<T>();
     }
+    public IQueryable<T> FindByCondition(Expression<Func<T, bool>> expression, bool trackChanges)
+    {
+        return !trackChanges
+            ? _context.Set<T>().Where(expression).AsNoTracking()
+            : _context.Set<T>().Where(expression);
+    }
+    #endregion
+
+    #region Write Methods (Veri ekleme, güncelleme, silme işlemleri)
+    public void Create(T entity)
+    {
+        _context.Set<T>().Add(entity);
+    }
+    public void Update(T entity)
+    {
+        _context.Set<T>().Update(entity);
+    }
+    public void Delete(T entity)
+    {
+        _context.Set<T>().Remove(entity);
+    }
+    #endregion
 }
--- Backend/SpendTracker.Repositories/Interfaces/IBaseRepository.cs
+++ Backend/SpendTracker.Repositories/Interfaces/IBaseRepository.cs
@@ -1,6 +1,56 @@
-﻿namespace SpendTracker.Repositories.Interfaces;
+﻿using System.Linq.Expressions;
+
+namespace SpendTracker.Repositories.Interfaces;
 
 public interface IBaseRepository<T> where T : class
 {
+    /// <summary>
+    /// Bütün kayıtları getirecek sorguyu oluşturur.
+    /// </summary>
+    /// <param name="trackChanges">
+    /// Verilerin değişiklik takibinin (Change Tracker) yapılıp yapılmayacağını belirler.
+    /// Sadece okuma işlemleri için 'false' verilmesi performansı artırır.
+    /// </param>
+    /// <returns>
+    /// Filtreleme veya sıralama eklenebilecek temel sorgu.
+    /// </returns>
     IQueryable<T> FindAll(bool trackChanges);
+
+    /// <summary>
+    /// Verilen koşula (expression) uyan kayıtları getirmek üzere sorguyu hazırlar.
+    /// </summary>
+    /// <param name="expression">
+    /// Veritabanı sorgusunda 'WHERE' şartı olarak işlenecek lambda ifadesi.
+    /// </param>
+    /// <param name="trackChanges">
+    /// Verilerin değişiklik takibinin (Change Tracker) yapılıp yapılmayacağını belirler.
+    /// Sadece okuma işlemleri için 'false' verilmesi performansı artırır.
+    /// </param>
+    /// <returns>
+    /// Koşula uyan, üzerinde zincirleme işlem (OrderBy, Select vb.) yapılabilecek sorgu sonucu.</returns>
+    IQueryable<T> FindByCondition(Expression<Func<T, bool>> expression, bool trackChanges);
+
+    /// <summary>
+    /// Veritabanına eklenmek üzere yeni bir nesneyi işaretler (State: Added).
+    /// </summary>
+    /// <param name="entity">
+    /// Veritabanına kaydedilecek olan varlık (entity).
+    /// </param>
+    void Create(T entity);
+
+    /// <summary>
+    /// Veritabanında güncellenmek üzere bir nesneyi işaretler (State: Modified).
+    /// </summary>
+    /// <param name="entity">
+    /// Veritabanında güncellenecek olan varlık (entity).
+    /// </param>
+    void Update(T entity);
+
+    /// <summary>
+    /// Veritabanından silinmek üzere bir nesneyi işaretler (State: Deleted).
+    /// </summary>
+    /// <param name="entity">
+    /// Veritabanından silinecek olan varlık (entity).
+    /// </param>
+    void Delete(T entity);
 }
